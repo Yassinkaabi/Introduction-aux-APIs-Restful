9b285518e7579e972fdb110c3c8816af4435530b
yassine <yassine.kaabi@sastec-group.com>
2024-01-27 12:42:50 +0100

    modifier la fonction getWeatherData ()

--- fetch.js
+++ fetch.js
@@ -4,14 +4,10 @@ const API_KEY = "38f9264b8e345e5059d64b5e08c19663";
 const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&lang=fr&units=metric&q=`;
 
 async function getWeatherData(city) {
-    try {
-        const url = BASE_URL + city;
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw error;
-    }
+    const url = BASE_URL + city;
+    return fetch(url)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
 }
 
 async function main() {
